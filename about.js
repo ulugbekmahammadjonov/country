0419907acc35721fdd34f9f20db4b4eb1d4fb9de
ulugbekmahammadjonov <ulugbekmahammadjonov1218@gmail.com>
2022-02-10 17:40:12 +0500

    style boshlandi

--- about.js
+++ about.js
@@ -33,9 +33,11 @@ function showResult(data){
         
         const div = document.createElement('div')
         let detail = `
-            <div class="card" style="width: 18rem;">
-                <img src="${flags.png}" class="card-img-top" alt="...">
-                <div class="card-body">
+            <div class="row container">
+               <div class="col-md-6"> 
+                    <img src="${flags.png}" class="card-img-top" alt="...">
+               </div>
+                <div class="card-body col-md-6">
                     <h5 class="card-title">${name.common}</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
--- app.js
+++ app.js
@@ -1,87 +1,124 @@
- const api ='https://restcountries.com/v3.1/all'
- const info = document.querySelector('.info')
- const cards= document.querySelector('.card-container');
- const card = document.querySelector('.card')
- const searchbar = document.querySelector('#searchbar');
+const url = 'https://restcountries.com/v3.1/all'
+const loadingCard = document.querySelector('.loading')
+const cards = document.querySelector('.card-container');
+const card = document.querySelector('.card')
+const searchbar = document.querySelector('#searchbar');
 const searchForm = document.querySelector('#search-form')
 const select = document.querySelector('.select-region')
-let loading=false
 let fullData = [];
 
- async function requestAPI(url){
+loading(true);
+function loading(isLoading) {
+    if (isLoading) {
+        loadingCard.innerHTML = `
+            <div class="d-flex justify-content-center mt-5">
+            <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+        `
+    }
+    else {
+        loadingCard.innerHTML = "";
+    }
+}
+
+async function requestAPI(url) {
     //  document.querySelector('.card').textContent='loading'
-    loading=true
-    try{
+    try {
         const request = await fetch(url)
-        if(!request.ok){
+        if (!request.ok) {
             throw new Error('Xatolik bor')
         }
-        const data = await request.json()
-        showResult(data)
-        fullData = data
-        loading=false
+        fullData = await request.json()
+        showResult(fullData)
+        loading(false)
     }
-    catch(err){
+    catch (err) {
         console.log(err.message)
     }
- }
+}
 
- requestAPI(api)
+requestAPI(url)
 
 
-function showResult(data){
-    const allCountry=data
-    allCountry.forEach((country)=>{
-        const {name, cioc, flags,region}=country
+function showResult(data) {
+    const allCountry = data;
+    cards.innerHTML="";
+    allCountry.forEach((country) => {
+        const { name, cioc, flags, population, region, capital} = country
         const div = document.createElement('div')
-        div.setAttribute('id',`${name.common}`)
-        div.setAttribute('data-set', `${region}`)
-        let card = `
+        div.setAttribute('id', `${name.common}`)
+    
+        div.innerHTML = `
             <div class="card" style="width: 18rem;">
                 <img src="${flags.png}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${name.common}</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="./about.html?${cioc}" class="btn btn-primary">Go somewhere</a>
+                    <p class="card-text">Population: ${population}</p>
+                    <p class="card-text">Region: ${region}</p>
+                    <p class="card-text">Capital: ${capital}</p>
+                    <div class="text-center">
+                        <a href="./about.html?${cioc}" class="btn btn-primary mx-auto">ReadMore</a>
+                    </div>
                 </div>
             </div>
         `;
-        div.innerHTML=card;
-        
         cards.appendChild(div);
     })
-    
+
 }
 
- 
+// searchbar.addEventListener('input', (e) => {
+//     let searchStr = e.target.value.toLowerCase()
+//     for (i = 0; i < cards.childNodes.length; i++) {
+//         const country = cards.childNodes[i].getAttribute('id').toLowerCase()
+//         if (!country.includes(searchStr)) {
+//             cards.childNodes[i].classList.add("hidden")
+
+//         } else {
+//             cards.childNodes[i].classList.remove("hidden")
+//         }
+//     }
+// })
+
+
 
 searchbar.addEventListener('input', (e) => {
-    let searchStr =e.target.value.toLowerCase()
-    for(i=1;i<cards.childNodes.length;i++){
-       const country = cards.childNodes[i].getAttribute('id').toLowerCase()
-       if(!country.includes(searchStr)){
-           cards.childNodes[i].classList.add("hidden")
-    
-       }else{
-        cards.childNodes[i].classList.remove("hidden")
-       }
-
-
+    let searchStr = e.target.value.toLowerCase()
+    let filteredData = [];
+    if (searchStr == "") {
+        filteredData = fullData;
     }
-    
+    else {
+        fullData.forEach((country) => {
+           if (country.name.common.toLowerCase().includes(searchStr)) {
+            filteredData.push(country);
+           }
+        })
+    }
+    showResult(filteredData);
 })
 
-select.addEventListener('change',(e)=>{
-    const region = select.value.toLowerCase()
-    for(i=1;i<cards.childNodes.length;i++){
-        const country = cards.childNodes[i].getAttribute('data-set').toLowerCase()
-        if(!country.includes(region)){
-            cards.childNodes[i].classList.add("hidden")
-     
-        }else{
-         cards.childNodes[i].classList.remove("hidden")
-        }
- 
- 
-     }
+select.addEventListener('change', (e) => {
+    const region = e.target.value.toLowerCase()
+    filterByRegion(region);
 })
+
+
+function filterByRegion(regionText) {
+    let filteredData = [];
+    if (regionText == "all") {
+        filteredData = fullData;
+    }
+    else {
+        fullData.forEach((country) => {
+            if(country.region.toLowerCase() == regionText.toLowerCase()) {
+                filteredData.push(country)
+            }
+        });
+       
+    }
+    showResult(filteredData);
+
+}
